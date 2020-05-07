6d220a3b8175aec25a816ca5dd79373f12405533
jetlime <paul.houssel@yahoo.de>
2020-05-07 17:55:43 +0200

    Zooming on image is possible

--- app/src/main/java/com/example/smartgarden/myPlants.java
+++ app/src/main/java/com/example/smartgarden/myPlants.java
@@ -1,11 +1,16 @@
 package com.example.smartgarden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -19,7 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class myPlants extends AppCompatActivity {
-
+    boolean isImageFitToScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +34,9 @@ public class myPlants extends AppCompatActivity {
 
         TextView itemName = (TextView) findViewById(R.id.itemName);
         TextView itemDescription = (TextView) findViewById(R.id.itemDescription);
-        ImageView itemPicture = (ImageView) findViewById(R.id.itemPicture);
-
+        final ImageView itemPicture = (ImageView) findViewById(R.id.itemPicture);
+        // fetch json in order to get information about the clicked plant
+        // the position of the clicked plant in the json file is stored in the variable "position"
         try {
             JSONObject json = new JSONObject(loadJSONFromInternal());
             JSONArray plantArray = json.getJSONArray("plants");
@@ -47,7 +53,26 @@ public class myPlants extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        // add click listener to image
+        // if the image is clicked, the image is zoomed in.
+        itemPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isImageFitToScreen) {
+                    isImageFitToScreen=false;
+                    itemPicture.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+                    itemPicture.setAdjustViewBounds(true);
+                }else{
+                    isImageFitToScreen=true;
+                    itemPicture.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+                    itemPicture.setScaleType(ImageView.ScaleType.FIT_XY);
+                }
+            }
+
+        });
+
     }
+
     public String loadJSONFromInternal() {
         // init object json of type string
         String json = null;
