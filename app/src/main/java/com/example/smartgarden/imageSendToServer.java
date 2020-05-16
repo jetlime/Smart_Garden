5e34b572bce2a86a1d89cc2597137e1b4aa44956
jetlime <paul.houssel@yahoo.de>
2020-05-16 12:26:04 +0200

    sending image to server done

--- app/src/main/java/com/example/smartgarden/imageSendToServer.java
+++ app/src/main/java/com/example/smartgarden/imageSendToServer.java
@@ -6,6 +6,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,7 +16,13 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.internal.http.multipart.MultipartEntity;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,10 +30,14 @@ import java.io.FileNotFoundException;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 
 import static com.android.volley.Request.Method.POST;
@@ -35,6 +47,8 @@ public class imageSendToServer extends AppCompatActivity {
     ProgressDialog dialog = null ;
     String path = "data/data/com.example.smartgarden/app_imageDir";
     int serverResponseCode;
+    ImageView imageSentToServer;
+    String base64string;
 
     public imageSendToServer() throws MalformedURLException {
 
@@ -44,7 +58,7 @@ public class imageSendToServer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_send_to_server);
-        ImageView imageSentToServer = (ImageView) findViewById(R.id.TakenImage);
+        imageSentToServer = (ImageView) findViewById(R.id.TakenImage);
         loadImageFromStorage(path);
         Button backHome = (Button) findViewById(R.id.backhome);
         final TextView status = (TextView) findViewById(R.id.plantStatus);
@@ -54,28 +68,24 @@ public class imageSendToServer extends AppCompatActivity {
                 openHome();
             }
         });
+        File f=new File(path, "profile.jpg");
 
-        // send image to server
-
-        new Thread(new Runnable() {
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    public void run() {
-
-                    }
-                });
-
-                try {
-                    uploadFile();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }).start();
-
+        URL url = null;
+        try {
+            url = new URL("http://messir.uni.lu/bicslab");
+            HttpURLConnection httpCon = null;
+            httpCon = (HttpURLConnection) url.openConnection();
+            httpCon.setDoOutput(true);
+            httpCon.setRequestMethod("PUT");
+            OutputStreamWriter out = null;
+            out = new OutputStreamWriter(httpCon.getOutputStream());
+            out.write(String.valueOf(f));
+            out.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         final Thread thread = new Thread(new Runnable() {
             @Override
@@ -98,17 +108,12 @@ public class imageSendToServer extends AppCompatActivity {
 
                         in.close();
                     } catch (MalformedURLException e) {
-
                     } catch (IOException e) {
-
-
                     }
                 }catch (Exception e) {
                     e.printStackTrace();
                 }
-
             }
-
         });
 
         thread.start();
@@ -119,9 +124,7 @@ public class imageSendToServer extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void loadImageFromStorage(String path)
-    {
-
+    private void loadImageFromStorage(String path) {
         try {
             File f=new File(path, "profile.jpg");
             Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
@@ -132,132 +135,6 @@ public class imageSendToServer extends AppCompatActivity {
         {
             e.printStackTrace();
         }
-
     }
 
-    public void uploadFile() throws IOException {
-
-
-        String fileName = path + "profile.jpg" ;
-        upLoadServerUri = "localhost:3000/image";
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        File sourceFile =new File(fileName);
-        // open a URL connection to the Servlet
-        FileInputStream fileInputStream = new FileInputStream(sourceFile);
-        URL url = new URL(upLoadServerUri);
-
-
-
-        try {
-
-            // Open a HTTP  connection to  the URL
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setDoInput(true); // Allow Inputs
-            conn.setDoOutput(true); // Allow Outputs
-            conn.setUseCaches(false); // Don't use a Cached Copy
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-            conn.setRequestProperty("uploaded_file", fileName);
-
-
-                dos = new DataOutputStream(conn.getOutputStream());
-
-                dos.writeBytes(twoHyphens + boundary + lineEnd);
-
-
-                   dos.writeBytes(lineEnd);
-
-                   // create a buffer of  maximum size
-                   bytesAvailable = fileInputStream.available();
-
-                   bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                   buffer = new byte[bufferSize];
-
-                   // read file and write it into form...
-                   bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                   while (bytesRead > 0) {
-
-                     dos.write(buffer, 0, bufferSize);
-                     bytesAvailable = fileInputStream.available();
-                     bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                     bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                    }
-
-                   // send multipart form data necesssary after file data...
-                   dos.writeBytes(lineEnd);
-                   dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                   // Responses from the server (code and message)
-                   serverResponseCode = conn.getResponseCode();
-                   Toast.makeText(imageSendToServer.this, serverResponseCode, Toast.LENGTH_LONG).show();
-                   String serverResponseMessage = conn.getResponseMessage();
-
-
-
-                   if(serverResponseCode == 200){
-
-                       runOnUiThread(new Runnable() {
-                            public void run() {
-
-                                Toast.makeText(imageSendToServer.this, "File Upload Complete.",
-                                             Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                   } else {
-                       runOnUiThread(new Runnable() {
-                           public void run() {
-
-                               Toast.makeText(imageSendToServer.this, "File Upload Failed, server not responding.",
-                                       Toast.LENGTH_SHORT).show();
-                           }
-                       });
-                   }
-
-                   //close the streams //
-                   fileInputStream.close();
-                   dos.flush();
-                   dos.close();
-
-              } catch (MalformedURLException ex) {
-
-
-                  ex.printStackTrace();
-
-                  runOnUiThread(new Runnable() {
-                      public void run() {
-
-                          Toast.makeText(imageSendToServer.this, "MalformedURLException",
-                                                              Toast.LENGTH_SHORT).show();
-                      }
-                  });
-
-              } catch (Exception e) {
-
-
-                  e.printStackTrace();
-
-                  runOnUiThread(new Runnable() {
-                      public void run() {
-                          Toast.makeText(imageSendToServer.this, "Got Exception",
-                                  Toast.LENGTH_SHORT).show();
-                      }
-                  });
-
-              }
-
-              //return serverResponseCode;
-
-
-         }
 }
